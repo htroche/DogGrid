df77332d850ea2059c1ab1a85418202cc4c5c701
Hugo Troche <hugo.founder@gmail.com>
2017-11-03 16:28:55 -0700

    Initial Commit

---
+++ DogGrid/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DogGrid
+//
+//  Created by Hugo Troche on 11/3/17.
+//  Copyright © 2017 Hugo Troche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ DogGrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DogGrid
+//
+//  Created by Hugo Troche on 11/3/17.
+//  Copyright © 2017 Hugo Troche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
